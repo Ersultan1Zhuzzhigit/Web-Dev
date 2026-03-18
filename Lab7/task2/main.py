09830505b8d7845bb4a4e463412e66fd7b4f5823
Ersultan1Zhuzzhigit <ersultanubt@gmail.com>
2026-03-18 23:20:38 +0600

    lab7

---
+++ Lab7/task2/main.py
@@ -0,0 +1,11 @@
+from models import Student, Teacher
+
+
+teacher1 = Teacher("Айсұлу", 35, "Математика", 10)
+student1 = Student("Әлихан", 14, 8)
+
+print(teacher1.introduce())  
+print(teacher1.teach())      
+
+print(student1.introduce())  
+print(student1.add_grade(5)) 
---
+++ Lab7/task2/models.py
@@ -0,0 +1,44 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        return f"Сәлем, менің атым {self.name}. Жасым {self.age}-де."
+
+    def get_role(self):
+        return "Мектеп мүшесі"
+
+    def __str__(self):
+        return f"{self.name} ({self.age} жас)"
+
+
+class Teacher(Person):
+    def __init__(self, name, age, subject, experience):
+        super().__init__(name, age)
+        self.subject = subject
+        self.experience = experience
+
+    def get_role(self): # type: ignore
+        return f"{self.subject} пәні мұғалімі"
+
+    def teach(self):
+        return f"{self.name} қазір {self.subject} сабағын түсіндіріп жатыр."
+
+
+class Student(Person):
+    def __init__(self, name, age, grade_level):
+        super().__init__(name, age)
+        self.grade_level = grade_level
+        self.grades = []
+
+    def get_role(self): # pyright: ignore[reportIncompatibleMethodOverride]
+        return f"{self.grade_level}-сынып оқушысы"
+
+    def add_grade(self, mark):
+        self.grades.append(mark)
+        return f"{mark} бағасы қойылды."
+
+    def introduce(self):
+        basic_intro = super().introduce()
+        return f"{basic_intro} Мен {self.grade_level}-сыныпта оқимын."
